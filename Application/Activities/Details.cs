4f7ef20dc158e9c6cd52bff8407e09fdf0d208a6
Csaba Földesi <csabus@gmail.com>
2024-04-29 16:29:07 +0200

    Mediator added

---
+++ Application/Activities/Details.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities;
+
+public class Details
+{
+    public class Query : IRequest<Activity>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, Activity>
+    {
+        private readonly DataContext _dataContext;
+
+        public Handler(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        async Task<Activity> IRequestHandler<Query, Activity>.Handle(
+            Query request,
+            CancellationToken cancellationToken
+        )
+        {
+            return await _dataContext.Activities.FindAsync(request.Id);
+        }
+    }
+}
